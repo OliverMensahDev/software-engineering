27c7a2736371c888d9e7081a46606ed75b37779e
Oliver Mensah <olivermensah96@gmail.com>
2018-08-21 18:25:29 +0000

    added

--- algorithms/js/illustrations/nextLetter.js
+++ algorithms/js/illustrations/nextLetter.js
@@ -1,14 +1,12 @@
 function letterChanges(str){
     return str.toLowerCase().replace(/[a-z]/gi, char =>{
-        if(char === 'z' || char ==='Z'){
+        if(char === 'z'){
             return 'a';
         }else{
             return String.fromCharCode(char.charCodeAt() +1)
         }
-    }).replace(/a|e|i|o|u/gi, vowel=>{
-        return vowel.toUpperCase();
-    })
+    });
 
 }
-console.log(letterChanges("I am"));
+console.log(letterChanges(""));
 
