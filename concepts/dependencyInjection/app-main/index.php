d447306cffae433018655790bf5736b9b1169c74
Oliver Mensah <olivermensah96@gmail.com>
2019-06-03 22:25:35 +0000

    Added

--- concepts/dependencyInjection/app-main/index.php
+++ concepts/dependencyInjection/app-main/index.php
@@ -7,6 +7,7 @@ use testapp\presentation\UserModel;
 use testapp\repositories\PersonRepository;
 use testapp\repositories\PersonCSVRepository;
 use testapp\repositories\PersonSQLRepository;
+use testapp\repositories\CachingRepository;
 use testapp\datastores\Database;
 use testapp\sharedObjects\Person;
 
@@ -16,7 +17,20 @@ $userModel = new UserModel(new PersonRepository);
 $userModel = new UserModel(new PersonCSVRepository("./dataPHP.csv"));
 $database =  new Database();
 $userModel = new UserModel(new PersonSQLRepository($database->get()));
+//caching
+// $cache = new CachingRepository(new PersonRepository);
+// $userModel = new UserModel($cache);
+// $cache->cachedData();
+
 $indexPage = new Template("./testapp/views/pages/index.php");
+
 // $userModel->addPerson(new Person("Oliver Mensah", "Male", 27));
 $indexPage->people = $userModel->getPeople();
+//simulateCaching
+// flush(); //this sends the output to the client. You may also need ob_flush();
+// sleep(10); 
+// $cache->invalidateCache();
+// $indexPage->people = $userModel->getPeople();
+
+
 echo $indexPage;
