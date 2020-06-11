340bcc855b10ddb158301b27b92e829ac3f06664
Oliver Mensah <olivermensah96@gmail.com>
2020-06-11 18:16:19 +0000

    Add user relationships to models

---
+++ programming-languages/Nodejs/Express/Authentication-Users-Permissions/05-user-relationship-to-other-models/routes/auth.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { register, login, getMe } = require('../controllers/auth');
+const { protectRoute } = require('../middleware/auth');
+
+const router = express.Router();
+
+router.post('/register', register);
+router.post('/login', login);
+router.get('/me', protectRoute, getMe);
+
+module.exports = router;
