e9012998ec102d4d4704234ae80b5c71083fa1eb
Oliver Mensah <olivermensah96@gmail.com>
2020-06-26 13:29:59 +0000

    decorator pattern  modified

--- software-engineering-concepts/design_patterns/php/DecoratorPattern/index.php
+++ software-engineering-concepts/design_patterns/php/DecoratorPattern/index.php
@@ -28,7 +28,6 @@ class VeggieParadise implements Pizza
 // Base Decorator
 class PizzaToppings implements Pizza
 {
-
     protected $pizza;
 
     public function __construct(Pizza $pizza)
@@ -61,7 +60,6 @@ class Jalapeno extends PizzaToppings
     }
 }
 
-
 function makePizza(Pizza $pizza)
 {
     echo "Your Order: " . $pizza->getDesc();
@@ -70,11 +68,10 @@ function makePizza(Pizza $pizza)
 $pizza = new Margherita();
 $pizza = new ExtraCheese($pizza);
 $pizza = new Jalapeno($pizza);
-
 makePizza($pizza);
 
-// New Twists 
 
+// Coffee
 interface Coffee
 {
     public function getCost();
@@ -212,3 +209,65 @@ class Decorator implements Coffee
 
 $decorator = new Decorator(5, 'Vanilla', new Decorator(5, 'Whip', new Decorator(2, 'Milk', new SimpleCoffee())));
 $decorator->print();
+
+
+
+//FizzBuzz
+interface Rule
+{
+    function print(int $number, bool $isMatch);
+}
+
+
+class NonMatchedRule implements Rule
+{
+    public function print(int $number, bool $isMatched)
+    {
+        if (!$isMatched) {
+            echo $number;
+        }
+        echo PHP_EOL;
+    }
+}
+
+class MatchedRule implements Rule
+{
+    private $name;
+    private $number;
+    private $nested_rule;
+
+    public function __construct(int $number, string $name, Rule $rule)
+    {
+        $this->number = $number;
+        $this->name = $name;
+        $this->nested_rule = $rule;
+    }
+
+    public function print(int $number, bool $isMatched)
+    {
+        if ($number % $this->number == 0) {
+            echo $this->name;
+            $this->nested_rule->print($number, true);
+        } else {
+            $this->nested_rule->print($number, $isMatched);
+        }
+    }
+}
+
+$FizzBuzzBazz = new MatchedRule(
+    3,
+    'Fizz',
+    new MatchedRule(
+        5,
+        "Buzz",
+        new MatchedRule(
+            7,
+            "Bazz",
+            new NonMatchedRule()
+        )
+    )
+);
+
+for ($i = 1; $i < 100; $i++) {
+    $FizzBuzzBazz->print($i, false);
+}
