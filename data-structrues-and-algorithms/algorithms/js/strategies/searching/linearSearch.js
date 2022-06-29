2d028a22fc7a39693348306e372e0d9694244179
Oliver Mensah <olivermensah96@gmail.com>
2022-06-29 08:04:23 +0000

    updated the data-structures and algorithm folder

--- data-structrues-and-algorithms/algorithms/js/strategies/searching/linearSearch.js
+++ data-structrues-and-algorithms/algorithms/js/strategies/searching/linearSearch.js
@@ -6,4 +6,28 @@ function seqSearch(arr, data){
     }
 }
 
+function findMin(arr){
+    var min= arr[0];
+    for(var i=1; i< arr.length; i++){
+        if(arr[i]<min){
+            min = arr[i]
+        }
+    }
+    return min;
+}
+
+console.log(findMin([1,0, 3, 9, -1]));
+
+function findMax(arr){
+    var max = arr[0];
+    for(var i=1; i< arr.length; i++){
+        if(arr[i]> max){
+            max = arr[i]
+        }
+    }
+    return max;
+}
+console.log(findMax([1,0, 3, 9, -1]));
+
+
 console.log(seqSearch([2,4,5,8,10,100], 10))
